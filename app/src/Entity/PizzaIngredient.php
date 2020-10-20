a3e5000747e411351f1bdea413a7a45586b8eff5
Carlos <bcode@protonmail.com>
2020-10-20 14:05:53 +0200

    Add pizzaIngredient entity  and  repository

--- app/src/Entity/PizzaIngredient.php
+++ app/src/Entity/PizzaIngredient.php
@@ -1,3 +1,41 @@
 <?php
 
-declare(strict_types = 1); 
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pizza
+ * @ORM\Entity
+ * @ORM\Table(name="pizzaIngredient")
+ * @ORM\Entity(repositoryClass="App\Repository\PizzaIngredientRepository")
+ */
+class PizzaIngredient
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="order",type="integer")
+     */
+    private $order;
+
+    /**
+     * @var pizza
+     * @ORM\ManyToOne(targetEntity="Pizza")
+     */
+    private $pizza;
+
+    /**
+     * @var Ingredient
+     * @ORM\ManyToOne(targetEntity="Ingredient")
+     */
+    private $ingredient;
+
+}
